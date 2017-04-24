4ce0676966d0b64f2cfa69e91a0403b2a32691d1
Kris Smith <kris@clubmanagercentral.com>
2017-04-24 17:20:38 +0100

    Initial commit

---
+++ ToDo/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDo.Models;
+
+
+namespace ToDo.Controllers
+{
+    public class TodoController : Controller
+    {
+        // GET: Todo
+        public ActionResult Index()
+        {
+            var todo = new Todo() {Name = "Test to do item" , Description="I need to WRITE the To do program"};
+            return View(todo);
+        }
+    }
+}
---
+++ ToDo/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Boolean Complete { get; set; }
+    }
+}
